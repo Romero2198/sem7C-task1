853a2602dff47bdb1e723670dd7bca53bb6fa036
Yung.Energy <morskojroma@gmail.com>
2022-07-23 22:35:48 +0300

    task1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿int m = InputInt("Введите количество строк: ");
+int n = InputInt("Введите количество столбцов: ");
+double[,] numbers = new double[m, n];
+FillArray(numbers);
+PrintArray(numbers);
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(Math.Round(array[i, j], 1) + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            numbers[i, j] = new Random().NextDouble() + new Random().Next(0, 10);
+        }
+    }
+}
